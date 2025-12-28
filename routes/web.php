0d5d62f2c9576702e60c7b99e41e5b27d9156ada
[Jose <[jjmartins99@hotmail.com]>
2025-12-28 02:33:42 +0100

    configurar Docker

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// routes/web.php
+Route::get('/health', function() {
+    return response()->json([
+        'status' => 'ok',
+        'services' => [
+            'database' => DB::connection()->getPdo() ? 'connected' : 'failed',
+            'redis' => Redis::connection()->ping() ? 'ok' : 'failed'
+        ]
+    ]);
+});
+
